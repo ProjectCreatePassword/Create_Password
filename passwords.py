7d469672619b0f491b2a9b62d4189d2484dcd986
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-03-15 00:55:38 +0100

    ajout de la comparaison des mots de passe hachés

--- passwords.py
+++ passwords.py
@@ -6,6 +6,7 @@ import string
 import random
 from hashlib import sha512
 import json
+import os
 
 
 message_erreur = """Votre mot de passe doit contenir
@@ -15,8 +16,41 @@ au moins:
 - Une majuscule
 - Une minuscule
 - Un chiffre"""
+
+message_correct = "Votre mot de passe est correct"
+
+message_deja_existant = "Le mot de passe existe déjà"
 #-------------------------------------------------------
 #Ajout fonctions
+def checkpassword(mdp):
+		majuscules = string.ascii_uppercase
+		minuscules = string.ascii_lowercase
+		chiffre = string.digits
+		spécial = string.punctuation
+		maj_présente = False
+		min_présente = False
+		chiffre_présent = False
+		spécial_présent = False
+		#On vérifie si le mot de passe est supérieur à 8
+		if len(mdp) >= 8:
+			for char in mdp:
+				#On vérifie que le mdp contienne au moins une majuscule
+				if char in majuscules:
+					maj_présente = True
+				#On vérifie que le mdp contienne au moins une minuscule
+				if char in minuscules:
+					min_présente = True
+				#On vérifie que le mdp contienne au moins un nombre
+				if char in chiffre:
+					chiffre_présent = True
+				#On vérifie que le mdp contienne au moins un caractère spécial
+				if char in spécial:
+					spécial_présent = True
+
+			return maj_présente and min_présente and chiffre_présent and spécial_présent
+		return False
+
+
 def ispasswordtrue():
 
 	def checkpassword():
@@ -50,16 +84,13 @@ def ispasswordtrue():
 
 	if checkpassword():
 		motdepasse = entrer_mdp.get()
+		validation.configure(text=message_correct, fg="green")
 		HashPassword(motdepasse)
 		entrer_mdp.delete(0, 100)
-		erreur.configure(text="")
 	else:
-
-		message = "Mot de passe incorrect"
 		entrer_mdp.delete(0, 100)
-		entrer_mdp.insert(0, message)
-		erreur.grid(column=1, row=1)
-		erreur.configure(text=message_erreur)
+		validation.grid(column=1, row=1)
+		validation.configure(text=message_erreur, fg="red")
 
 
 def genpassword():
@@ -69,31 +100,71 @@ def genpassword():
 	symbole = string.punctuation
 	caractère = lettre_min + lettre_maj + symbole + chiffre
 	mdp = ""
-	for i in range(0, 15):
-		cmdp = random.choice(caractère)
-		mdp = mdp + cmdp
-	".join return mdp"
-	entrer_mdp.delete(0, 100)
-	entrer_mdp.insert(0, mdp)
+	checkpassword(mdp)
+	while not checkpassword(mdp):
+		mdp= ""
+		for i in range(0, 15):
+			cmdp = random.choice(caractère)
+			mdp = mdp + cmdp
+			".join return mdp"
+			checkpassword(mdp)
+	else:
+		entrer_mdp.delete(0, 100)
+		entrer_mdp.insert(0, mdp)
+
 
 def HashPassword(motdepasse):
 	motdepasse = motdepasse.encode()
 	motdepasse_sign = sha512(motdepasse).hexdigest()
-	motdepasse_crypté = [motdepasse_sign] 
 	fichier = "passwords.json"
-	with open(fichier, "a") as file:
-		json.dump(motdepasse_crypté, file)
-		file.write("\n")
+	list_mdp_cryptés = []
+	if os.path.getsize(fichier) > 0:
+		#On charge les mots de passe déjà existants
+		with open(fichier, "r") as file:
+			list_mdp_cryptés = json.load(file)
+		if motdepasse_sign in list_mdp_cryptés:
+			validation.grid(column=1, row=1)
+			validation.configure(text=message_deja_existant)
+		else:
+			#On ajoute le nouveau mot de passe crypté à la liste
+			list_mdp_cryptés.append(motdepasse_sign)
+			#On réécrit le fichier avec le nouveau mot de passe
+			with open(fichier, "w") as file:
+				json.dump(list_mdp_cryptés, file)
+	else:
+		with open(fichier, "w") as file:
+			json.dump([motdepasse_sign], file)
 
 def ShowPasswords():
 	fichier = "passwords.json"
 	with open(fichier, "r") as file:
-		mdpcryptés.delete(1.0, 1000.0)
-		for line in file:
-			mdp = json.loads(line)
+		mdpcryptés.delete(1.0, "end")
+		mdp = json.load(file)
+		for element in mdp:
 			mdpcryptés.grid(column=0, columnspan=3)
 			mdpcryptés.configure(height=100, width=600)
-			mdpcryptés.insert(1.0, str(mdp)+'\n')
+			mdpcryptés.insert(1.0, (f"[ {element} ]\n________________________________________________________________________________________________\n\n"))
+
+def toggle_visibility():
+    if voir_mdp.get():
+        entrer_mdp.configure(show="")
+    else:
+        entrer_mdp.configure(show="*")
+
+def DidPasswordAlreadyExist():
+	fichier = "passwords.json"
+	list_mdp_cryptés = []
+	new_mdp = []
+	new_mdp.append(motdepasse_sign)
+	with open(fichier, "r") as file:
+		for line in file:
+			mdp = json.loads(line)
+			list_mdp_cryptés.append(mdp)
+			for password in new_mdp:
+				if new_mdp in list_mdp_cryptés:
+					print("deja la")
+				else:
+					print("yeaaah")
 
 
 #--------------------------------------------
@@ -135,34 +206,34 @@ contenu = Frame(window, bg="#181A1B")
 contenu.grid(pady=10, column=1)
 
 #-------------------------------------------------------
-#---------Message d'erreur
-erreur = Label(window, font=("Courrier", 10), bg="#181A1B", fg="red", justify="left", anchor="w")
+#---------Message de validationvalidation
+validation = Label(window, font=("Courrier", 10), bg="#181A1B", justify="left", anchor="w")
 
 #--------------------------------------------
 #------------Input mdp
 entrer_mdp = customtkinter.CTkEntry(master=contenu, show="*", bg_color="#181A1B", fg_color="#4c4f4c", corner_radius=8, width=200, placeholder_text="Entrez un mot de passe")
-entrer_mdp.grid(pady=10, row=1) 
+entrer_mdp.grid(pady=5, row=1) 
 
 # Bouton pour rendre le mdp visible ou masqué
 voir_mdp = BooleanVar()
 voir_mdp.set(False)  # Mdp masqué par défaut
-show_button = Checkbutton(contenu, text="Cliquez pour afficher le mot de passe", variable=voir_mdp, command=toggle_visibility, bg="#C8ADC0")
-show_button.grid()
+show_button = customtkinter.CTkCheckBox(master=window, text="Afficher le mot de passe", variable=voir_mdp, command=toggle_visibility, fg_color="white", border_width=2, border_color="#ff0000", corner_radius=8)
+show_button.grid(column=2, row=2)
 
 #--------------------------------------------
 #------------Bouton générer mdp
 generate = customtkinter.CTkButton(master=contenu, bg_color="#181A1B", fg_color="#631CD0", corner_radius=8, width=200 , text="Générer un mot de passe", command=genpassword)
-generate.grid(pady=10, row=2)
+generate.grid(pady=5, row=2)
 
 #--------------------------------------------
 #------------Bouton envoyer mdp
 envoyer = customtkinter.CTkButton(master=contenu, bg_color="#181A1B", fg_color="#78BB7B", corner_radius=8, width=200, text="Envoyer", command=ispasswordtrue)
-envoyer.grid(pady=10, row=3)
+envoyer.grid(pady=5, row=3)
 
 #--------------------------------------------
 #------------Bouton show mdp
 afficher = customtkinter.CTkButton(master=contenu, bg_color="#181A1B", fg_color="#0090ff", corner_radius=8, width=200, text="Afficher les mots de passe", command=ShowPasswords)
-afficher.grid(pady=10, row=4)
+afficher.grid(pady=5, row=4)
 
 #--------------------------------------------
 #------------Label show mdp
